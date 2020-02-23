5db5869ab3f5a1004b33f2255ddfa15257af3420
Sonya Jean <anawnx@gmail.com>
2020-02-23 01:07:43 -0800

    fixed deploy 3

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from distutils.command.config import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
